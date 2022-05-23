285cf6322bac7bfd640b0ea6bb824f2f9c38fe4e
Ali Hasan <ahb7671@gmail.com>
2022-05-23 10:02:42 +0500

    sweet: update from V13.0.8.0.SKFEUXM
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- proprietary/vendor/bin/install-recovery.sh
+++ proprietary/vendor/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:134217728:04708e0454885a1b9c2f4806c9c0e3e32463fc36; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:134217728:ceeb673bd5b733939e1c79f25021ee9a432346f4; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:98cef5f784194b14cf8617f8be510fe804a77959 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery:134217728:04708e0454885a1b9c2f4806c9c0e3e32463fc36 && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:c3d85375c515b245bb2f55ef5c535f0bc7f81949 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery:134217728:ceeb673bd5b733939e1c79f25021ee9a432346f4 && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
